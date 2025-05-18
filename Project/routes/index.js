89be69c9ad63d4667772ac95b575dc1a74c95adc
Luis Andrè Barrios Luna Victoria <luisbarriosaraujo@outlook.it>
2025-05-18 13:46:50 +0200

    fix responsive problem

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Pages/index', { title: 'Play Cards !!!' });
+  const cards = [
+    {
+      image: '/images/pinella.jpg',
+      title: 'Pinella',
+      description: 'Gioca a Pinella con le regole classiche!',
+      link: '/pinella'
+    },
+    {
+      image: '/images/cards.jpg',
+      title: 'Briscola',
+      description: 'Sfida il computer o un amico!',
+      link: '/briscola'
+    },
+    {
+      image: '/images/cards.jpg',
+      title: 'Poker',
+      description: 'Texas Hold\'em e altro ancora.',
+      link: '/poker'
+    }
+  ];
+  res.render('Pages/index', { cards: cards});
 });
 
 module.exports = router;
